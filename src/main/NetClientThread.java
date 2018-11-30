20330b5ccad71f021c209f6ac3127bce02a9444e
Popov Timofei <P_timofei@mail.ru>
2018-11-30 13:01:51 +0300

    added sout recipe

--- src/main/NetClientThread.java
+++ src/main/NetClientThread.java
@@ -1,63 +1,52 @@
 package main;
 
 import java.io.*;
-
 import java.net.*;
+import java.util.Scanner;
 
 public class NetClientThread {
-
     public static void main(String[] args) {
-
         try {
-
-// установка соединения с сервером
-
+            // установка соединения с сервером
             Socket s = new Socket(InetAddress.getLocalHost(), 8071);
-
-//или Socket s = new Socket("ИМЯ_СЕРВЕРА", 8071);
+            //или Socket s = new Socket("ИМЯ_СЕРВЕРА", 8071);
 
             PrintStream ps = new PrintStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            Scanner scanner = new Scanner(System.in);
+            //Растительное масло;
+            String request = "Яйцо куриное;Соль;Сахар;Молоко;Пшеничная мука;Гашеная сода;Растительное масло;";
 
-            BufferedReader br = new BufferedReader(
+//            System.out.println("Введите ингредиенты. Для поиска ничего не вводить.");
+//            String ing = scanner.nextLine();
+//            while (!ing.isEmpty()){
+//                request += ing + ";";
+//                ing = scanner.nextLine();
+//            }
+            System.out.println("Поиск рецепта по ингредиентам \n" + request);
+            ps.println(request);
 
-                    new InputStreamReader(s.getInputStream()));
-
-            for (int i = 1; i <= 10; i++) {
-
-                ps.println("PING");
-
-                System.out.println(br.readLine());
-
-                Thread.sleep(1000);
-
+            String[] recipes = br.readLine().split(";");
+            for (int i = 0; i < recipes.length; i++) {
+                System.out.println("*************************************");
+                System.out.println(recipes[i]);
             }
+
+            Thread.sleep(1000);
+
 
             s.close();
 
         } catch (UnknownHostException e) {
-
-// если не удалось соединиться с сервером
-
+            // если не удалось соединиться с сервером
             System.out.println("адрес недоступен");
-
             e.printStackTrace();
-
         } catch (IOException e) {
-
             System.out.println("ошибка I/О потока");
-
             e.printStackTrace();
-
         } catch (InterruptedException e) {
-
-            System.out.println(
-
-                    "ошибка потока выполнения");
-
+            System.out.println("ошибка потока выполнения");
             e.printStackTrace();
-
         }
-
     }
-
 }
